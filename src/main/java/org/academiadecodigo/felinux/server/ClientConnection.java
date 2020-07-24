6f44391562e76e761582fb3b1ba4573b452f324b
Pinhas <joao_pinheiro10@hotmail.com>
2020-07-24 20:11:42 +0100

    new branch to work on ClientConnection

--- src/main/java/org/academiadecodigo/felinux/server/ClientConnection.java
+++ src/main/java/org/academiadecodigo/felinux/server/ClientConnection.java
@@ -1,11 +1,16 @@
 package org.academiadecodigo.felinux.server;
 
+import org.academiadecodigo.felinux.controller.Controller;
+import org.academiadecodigo.felinux.model.client.Client;
+
 import java.net.Socket;
 
 public class ClientConnection implements Runnable {
 
     private Socket socket;
     private Server server;
+    private Client client;
+    private Controller controller;
 
 
     @Override
